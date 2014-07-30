7c9ac3578cc5c0e96a8fafb92cb23ab2a37b3617
John-Philip Johansson <seriema@gmail.com>
2014-07-30 18:25:24 -0300

    Integrate Wikipedia API.

--- app/scripts/controllers/found.js
+++ app/scripts/controllers/found.js
@@ -8,12 +8,16 @@
  * Controller of the translapediaApp
  */
 angular.module('translapediaApp')
-  .controller('FoundCtrl', function ($scope) {
-		$scope.fromLang = 'English';
-		$scope.term = 'Soursop';
-		$scope.fromTerm = 'Soursop';
-		$scope.toLang = 'Portuguese';
-		$scope.toTerm = 'Graviola';
-		$scope.desc = 'Soursop is the fruit of Annona muricata, a broadleaf, flowering, evergreen tree native to Mexico, Cuba, Central America, the Caribbean, and northern South America, primarily ...';
-		$scope.imageUrl = '//upload.wikimedia.org/wikipedia/commons/thumb/9/90/Annona_muricata_Blanco1.196.png/474px-Annona_muricata_Blanco1.196.png';
+  .controller('FoundCtrl', function ($scope, $rootScope) {
+		if ($rootScope.translation) {
+			angular.extend($scope, $rootScope.translation);
+		} else {
+			$scope.fromLang = 'English';
+			$scope.term = 'Soursop';
+			$scope.fromTerm = 'Soursop';
+			$scope.toLang = 'Portuguese';
+			$scope.toTerm = 'Graviola';
+			$scope.desc = 'Soursop is the fruit of Annona muricata, a broadleaf, flowering, evergreen tree native to Mexico, Cuba, Central America, the Caribbean, and northern South America, primarily ...';
+			$scope.imageUrl = '//upload.wikimedia.org/wikipedia/commons/thumb/9/90/Annona_muricata_Blanco1.196.png/474px-Annona_muricata_Blanco1.196.png';
+		}
   });
--- app/scripts/controllers/translate.js
+++ app/scripts/controllers/translate.js
@@ -8,11 +8,60 @@
  * Controller of the translapediaApp
  */
 angular.module('translapediaApp')
-  .controller('TranslateCtrl', function ($scope) {
-    $scope.fromLang = 'English';
-		$scope.fromTerm = 'Soursop';
+  .controller('TranslateCtrl', function ($scope, $rootScope, $http, $location) {
+    $scope.fromLangId = $location.search().from;
+		$scope.fromTerm = $location.search().term;
+		$scope.toLangId = $location.search().to;
+/*
+		$scope.fromLang = 'English';
 		$scope.toLang = 'Portuguese';
 		$scope.toTerm = 'Graviola';
 		$scope.desc = 'Soursop is the fruit of Annona muricata, a broadleaf, flowering, evergreen tree native to Mexico, Cuba, Central America, the Caribbean, and northern South America, primarily ...';
 		$scope.imageUrl = '//upload.wikimedia.org/wikipedia/commons/thumb/9/90/Annona_muricata_Blanco1.196.png/474px-Annona_muricata_Blanco1.196.png';
+*/
+		function onFail() {
+			$scope.fromLang = '';
+			$scope.fromTerm = '';
+			$scope.toLang = '';
+			$scope.toTerm = '';
+			$scope.desc = 'FAIL';
+			$scope.imageUrl = ''; // '//upload.wikimedia.org/wikipedia/commons/thumb/9/90/Annona_muricata_Blanco1.196.png/474px-Annona_muricata_Blanco1.196.png';
+		}
+
+		function onSuccess(response) {
+			console.log(response);
+			var pageId = response.data.query.pageids[0];
+			var article = response.data.query.pages[pageId];
+//			var imgRoot = '';
+			$rootScope.translation = {
+				originalTerm: $scope.fromTerm,
+				fromTerm: article.title,
+				fromDesc: article.extract,
+//				fromImg: imgRoot + article.images[0].title,
+				toTerm: article.langlinks[0]['*'],
+				toArticleUrl: article.langlinks[0].url,
+				toLangId: article.langlinks[0].lang
+			};
+
+			$location.path('/found');
+		}
+
+//		var apiUrl = '.wikipedia.org/w/api.php';
+//		$http.get('//' + $scope.fromLangId + apiUrl, {
+		$http.get('/mock_data/soursop.json', {
+			params: {
+				action: 'query',
+				prop: 'extracts|images|langlinks',
+				format: 'json',
+				exchars: 200,
+				exlimit: 1,
+				exintro: '',
+				imlimit: 1,
+				indexpageids: '',
+				lllimit: 10,
+				llprop: 'url',
+				lllang: $scope.toLangId,
+				titles: $scope.fromTerm
+			}
+		}).then(onSuccess, onFail);
   });
