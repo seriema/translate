493ee6c81b23738da63dfb1c352390918d4f3ce6
John-Philip Johansson <seriema@gmail.com>
2014-07-31 13:15:51 -0300

    Better data-usage on view 'not-found-translation'.

--- app/scripts/controllers/not-found-translation.js
+++ app/scripts/controllers/not-found-translation.js
@@ -8,9 +8,16 @@
  * Controller of the translapediaApp
  */
 angular.module('translapediaApp')
-  .controller('NotFoundTranslationCtrl', function ($scope) {
-		$scope.term = 'Soursop';
-		$scope.toLang = 'Portuguese';
-		$scope.desc = 'Soursop is the fruit of Annona muricata, a broadleaf, flowering, evergreen tree native to Mexico, Cuba, Central America, the Caribbean, and northern South America, primarily ...';
-		$scope.imageUrl = '//upload.wikimedia.org/wikipedia/commons/thumb/9/90/Annona_muricata_Blanco1.196.png/474px-Annona_muricata_Blanco1.196.png';
-  });
+  .controller('NotFoundTranslationCtrl', function ($scope, $rootScope) {
+		if (!$rootScope.translation) {
+			$scope.translation = {
+				fromLangId: 'en',
+				originalTerm: 'soursop',
+				fromTerm: 'Soursop',
+				toLangId: 'sv',
+				fromDesc: 'Soursop is the fruit of Annona muricata, a broadleaf, flowering, evergreen tree native to Mexico, Cuba, Central America, the Caribbean, and northern South America, primarily ...'
+//				imageUrl = '//upload.wikimedia.org/wikipedia/commons/thumb/9/90/Annona_muricata_Blanco1.196.png/474px-Annona_muricata_Blanco1.196.png';
+			};
+		}
+
+	});
