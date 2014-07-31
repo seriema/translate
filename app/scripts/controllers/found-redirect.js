aee0043054881c7cf1a683cf8bdda013953de420
John-Philip Johansson <seriema@gmail.com>
2014-07-31 12:18:39 -0300

    Better data-usage on view 'found-redirect'.

--- app/scripts/controllers/found-redirect.js
+++ app/scripts/controllers/found-redirect.js
@@ -8,13 +8,16 @@
  * Controller of the translapediaApp
  */
 angular.module('translapediaApp')
-  .controller('FoundRedirectCtrl', function ($scope) {
-		$scope.fromLang = 'English';
-		$scope.term = 'Trekking';
-		$scope.fromTerm = 'Trekking';
-		$scope.toLang = 'Portuguese';
-		$scope.toTerm = 'Trekkizinho';
-		$scope.originalTerm = 'trek';
-		$scope.desc = 'A trek is a long, adventurous journey undertaken on foot in areas where common means of transport are generally not available. Trekking should not be confused with mountaineering. In North America the equivalent is backpacking, while New Zealanders use the word tramping.';
-		$scope.imageUrl = '//upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Gorbeia_park.jpg/480px-Gorbeia_park.jpg';
+  .controller('FoundRedirectCtrl', function ($scope, $rootScope) {
+		if (!$rootScope.translation) {
+			$scope.translation = {
+				fromLangId: 'en',
+				originalTerm: 'trek',
+				fromTerm: 'Backpacking',
+				toLangId: 'sv',
+				toTerm: 'vandring',
+				fromDesc: 'Backpacking is generally an extended journey or walk with a backpack.[1] However, for North American hikers it more frequently describes a multi-day hike that involves camping, ...'
+//				imageUrl = '//upload.wikimedia.org/wikipedia/commons/thumb/9/90/Annona_muricata_Blanco1.196.png/474px-Annona_muricata_Blanco1.196.png';
+			};
+		}
 	});
